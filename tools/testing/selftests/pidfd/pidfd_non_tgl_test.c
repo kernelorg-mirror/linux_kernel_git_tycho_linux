58163e78cefa9aff8596bca4a765ac4a8305dedf
Tycho Andersen <tandersen@netflix.com>
2023-11-28 14:51:58 -0700

    clone: allow CLONE_THREAD | CLONE_PIDFD together
    This removes the restriction of CLONE_THREAD | CLONE_PIDFD being specified
    together. Assuming the previous patch sorts out all the thorny issues this
    should be safe. I've left it as a separate patch since it is not strictly
    necessary as a usecase for us, but might be nice? Perhaps we want to wait
    until someone actually needs it though.
    
    Signed-off-by: Tycho Andersen <tandersen@netflix.com>

--- tools/testing/selftests/pidfd/pidfd_non_tgl_test.c
+++ tools/testing/selftests/pidfd/pidfd_non_tgl_test.c
@@ -546,6 +546,22 @@ cleanup:
 	return ret;
 }
 
+static int test_clone_thread_pidfd(void)
+{
+	pid_t pid;
+	int flags = CLONE_THREAD | CLONE_VM | CLONE_SIGHAND | CLONE_PIDFD;
+	int pidfd;
+
+	pid = clone(thread_sleep, stack + STACK_SIZE, flags, NULL, &pidfd);
+	if (pid < 0) {
+		perror("clone");
+		return KSFT_FAIL;
+	}
+
+	close(pidfd);
+	return KSFT_PASS;
+}
+
 #define T(x) { x, #x }
 struct pidfd_non_tgl_test {
 	int (*fn)();
@@ -557,6 +573,7 @@ struct pidfd_non_tgl_test {
 	T(test_non_tgl_exit_poll),
 	T(test_non_tgl_waitid_exit),
 	T(test_non_tgl_waitid_exec),
+	T(test_clone_thread_pidfd),
 };
 #undef T
 
